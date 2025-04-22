08e3a388fafb6b019aa5df0ed51d1fd6d711dc53
Maharshi Das <maharshidas001@gmail.com>
2025-04-22 12:16:25 +0530

    Configure fonts

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./src/**/*.{html,js,jsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'work': ['Work Sans', 'sans-serif'],
+        'open': ['Open Sans', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
