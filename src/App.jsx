5f820909e01a03ff3f8cbd8b0529f74b7c173f08
Maharshi Das <maharshidas001@gmail.com>
2025-04-27 16:10:22 +0530

    Withoute Header Layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Routes, Route } from 'react-router-dom';
 import WithHeader from '@/layouts/WithHeader';
 import Home from '@/pages/Home';
 import Pricing from '@/pages/Pricing';
+import WithoutHeader from './layouts/WithoutHeader';
+import Dashboard from './pages/Dashboard';
 
 const App = () => {
   return (
@@ -11,6 +13,10 @@ const App = () => {
         <Route path="/" element={<WithHeader />}>
           <Route index element={<Home />} />
           <Route path='/pricing' element={<Pricing />} />
+        </Route>
+
+        <Route path="/dashboard" element={<WithoutHeader />}>
+          <Route index element={<Dashboard />} />
         </Route>
       </Routes>
     </>
