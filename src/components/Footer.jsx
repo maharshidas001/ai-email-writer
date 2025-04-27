6fcb43d7062d63133efa8dfd6dbe97aca2b256b2
Maharshi Das <maharshidas001@gmail.com>
2025-04-27 15:33:45 +0530

    Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MaxWidth from './MaxWidth';
+
+const Footer = () => {
+  return (
+    <>
+      <MaxWidth className='py-4 mt-8 bg-accent'>
+        <p className='text-center'>&copy; 2025 AI Email Writer. All rights reserved.</p>
+      </MaxWidth>
+    </>
+  )
+}
+
+export default Footer;
--- src/layouts/WithHeader.jsx
+++ src/layouts/WithHeader.jsx
@@ -1,3 +1,4 @@
+import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 import React from 'react';
 import { Outlet } from 'react-router-dom';
@@ -10,7 +11,7 @@ const WithHeader = ({ children }) => {
         {children}
         <Outlet />
       </main>
-      Footer
+      <Footer />
     </>
   )
 }
